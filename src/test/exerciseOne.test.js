e1032888c3f6d94b69f0c891c1ebc469a140ceab
henriqueeyer <henriqueeyer@outlook.com>
2020-05-13 05:34:21 -0300

    Adicionando cleanup

--- src/test/exerciseOne.test.js
+++ src/test/exerciseOne.test.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent, cleanup } from '@testing-library/react';
 import App from '../App';
 
+afterEach(cleanup);
+
+// arquivo App.test.js pode servir de exemplo
 describe('Testando a aplicação, testando botão, e sua funcionalidade', () => {
-  const { getByLabelText, getByText } = render(<App />);
-  const inputTask = getByLabelText('Tarefa:');
-  const labelTask = getByText('Tarefa:');
-  
   test('Verificando se o botão está na tela e se o ele contém o texto "Adicionar"', () => {
+    const {getByText} = render(<App />);
 
   });
 
@@ -15,6 +15,7 @@ describe('Testando a aplicação, testando botão, e sua funcionalidade', () => 
     Verificando sua funcionalidade, ao ser clicado é necessário adicionar o que o usuário digitou
   Use os fireEvent, para simular a digitação do usuário e o clique.
   `, () => {
+    const {getByLabelText,queryByText} = render(<App />);
 
   });
 });
