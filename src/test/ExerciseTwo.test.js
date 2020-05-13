542c7b4a0e6285b705ba22e9d4cb2575b0b5e51c
henriqueeyer <henriqueeyer@outlook.com>
2020-05-13 05:43:31 -0300

    Atualizando os testes

--- src/test/ExerciseTwo.test.js
+++ src/test/ExerciseTwo.test.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, cleanup } from '@testing-library/react';
 import App from '../App';
 import Item from '../Item';
 
-describe('Testando a aplicação, testando input', () => {
-  const { getByLabelText, getByRole } = render(<App />) // Caso precise de uma nova query adicione no object destructuring
-  const inputTask = getByLabelText('Tarefa:');
-  const btnAdd = getByRole('button');
+afterEach(cleanup);
 
+describe('Testando a aplicação, testando input', () => {
   test('Testando a adição de vários itens a aplicação', () => {
     const listTodo = ['Realizar CR', 'Ler Post no Medium', 'Beber água']; // Use esse array como base para realizar os testes.
+    const {} = render(<App />) // Caso precise de uma nova query adicione no object destructuring
 
   })
 });
